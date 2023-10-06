48ca68f0bec3c71c6d03e41f1cf42f64a11ae270
emmaroche <20088680@mail.wit.ie>
2023-10-06 17:09:40 +0100

    Complete lab-04

--- src/main/kotlin/ie/setu/controllers/HealthTrackerController.kt
+++ src/main/kotlin/ie/setu/controllers/HealthTrackerController.kt
@@ -1,4 +1,54 @@
 package ie.setu.controllers
 
-class HealthTrackerController {
+import ie.setu.domain.repository.UserDAO
+import io.javalin.http.Context
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
+import com.fasterxml.jackson.module.kotlin.readValue
+import ie.setu.domain.User
+
+object HealthTrackerController {
+
+    private val userDao = UserDAO()
+
+    fun getAllUsers(ctx: Context) {
+        ctx.json(userDao.getAll())
+    }
+
+    fun getUserByUserId(ctx: Context) {
+        val user = userDao.findById(ctx.pathParam("user-id").toInt())
+        if (user != null) {
+            ctx.json(user)
+        }
+    }
+
+    fun addUser(ctx: Context) {
+        val mapper = jacksonObjectMapper()
+        val user = mapper.readValue<User>(ctx.body())
+        userDao.save(user)
+        ctx.json(user)
+    }
+
+    fun getUserByEmail(ctx: Context) {
+        val email = ctx.pathParam("email")
+        val user = userDao.findByEmail(email)
+        if (user != null) {
+            ctx.json(user)
+        } else {
+            ctx.status(404).result("User not found")
+        }
+    }
+
+    fun deleteUser(ctx: Context) {
+        userDao.delete(ctx.pathParam("user-id").toInt())
+    }
+
+    fun updateUser(ctx: Context){
+        val mapper = jacksonObjectMapper()
+        val userUpdates = mapper.readValue<User>(ctx.body())
+        userDao.update(
+            id = ctx.pathParam("user-id").toInt(),
+            user=userUpdates)
+    }
+
+
 }
