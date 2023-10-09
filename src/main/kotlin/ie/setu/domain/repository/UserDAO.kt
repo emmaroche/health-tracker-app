785f30653108057cd291bd713a7c4d7466330ab6
emmaroche <20088680@mail.wit.ie>
2023-10-09 14:02:07 +0100

    Complete lab-05 step 12

--- src/main/kotlin/ie/setu/domain/repository/UserDAO.kt
+++ src/main/kotlin/ie/setu/domain/repository/UserDAO.kt
@@ -5,6 +5,8 @@ import ie.setu.domain.db.Users
 import ie.setu.utils.mapToUser
 import org.jetbrains.exposed.sql.selectAll
 import org.jetbrains.exposed.sql.transactions.transaction
+import org.jetbrains.exposed.sql.select
+
 class UserDAO {
 
     fun getAll(): ArrayList<User> {
@@ -17,8 +19,14 @@ class UserDAO {
     }
 
     fun findById(id: Int): User?{
-        return null
+        return transaction {
+            Users.select {
+                Users.id eq id}
+                .map{mapToUser(it)}
+                .firstOrNull()
+        }
     }
+
 
     fun save(user: User){
     }
