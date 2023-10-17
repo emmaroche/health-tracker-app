da9efb330ce323fb5d2c8cb42337f60cdad5e430
emmaroche <20088680@mail.wit.ie>
2023-10-17 15:42:49 +0100

    Add models for new features

---
+++ src/main/kotlin/ie/setu/domain/db/FitnessGoals.kt
@@ -0,0 +1,14 @@
+package ie.setu.domain.db
+
+import org.jetbrains.exposed.sql.ReferenceOption
+import org.jetbrains.exposed.sql.Table
+
+object FitnessGoals : Table("fitness-goals") {
+    val id = integer("id").autoIncrement().primaryKey()
+    val type = varchar("type", 50)
+    val workoutsPerWeek = integer("workouts_per_week")
+    val minutesOfWorkouts = integer("minutes_of_workouts")
+    val calorieBurningGoalDuringExercise = double("calorie_burning_goal")
+    val userId = integer("user_id").references(Users.id, onDelete = ReferenceOption.CASCADE)
+}
+
---
+++ src/main/kotlin/ie/setu/domain/db/HealthRecords.kt
@@ -0,0 +1,20 @@
+package ie.setu.domain.db
+
+import org.jetbrains.exposed.sql.ReferenceOption
+import org.jetbrains.exposed.sql.Table
+object HealthRecords : Table("health-records") {
+    val id = integer("id").autoIncrement().primaryKey()
+    val timestamp = varchar("timestamp", 100)
+    val firstName = varchar("first_name", 50)
+    val lastName = varchar("last_name", 50)
+    val sex = varchar("sex", 10)
+    val DOB = date("dob")
+    val weight = double("weight")
+    val height = integer("height")
+    val bloodType = varchar("blood_type", 5)
+    val allergies = varchar("allergies", 100)
+    val medicalConditions = varchar("medical_conditions", 100)
+    val medications = varchar("medications", 100)
+    val notes = varchar("notes", 200)
+    val userId = integer("user_id").references(Users.id, onDelete = ReferenceOption.CASCADE)
+}
