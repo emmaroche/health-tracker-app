458c3b0629df6e5a6bd50f44a6125069b4911388
emmaroche <20088680@mail.wit.ie>
2023-10-18 09:23:02 +0100

    Update Health Record files

--- src/main/kotlin/ie/setu/domain/HealthRecord.kt
+++ src/main/kotlin/ie/setu/domain/HealthRecord.kt
@@ -10,11 +10,11 @@ data class HealthRecord(
     val sex: String,
 //    val DOB: DateTime,
     val weight: Double,
-//    val height: Int,
-//    val bloodType: String,
-//    val allergies: String,
-//    val medicalConditions: String,
-//    val medications: String,
-//    val notes: String,
+    val height: Int,
+    val bloodType: String,
+    val allergies: String,
+    val medicalConditions: String,
+    val medications: String,
+    val notes: String,
     var userId: Int
 )
--- src/main/kotlin/ie/setu/utils/DBUtilities.kt
+++ src/main/kotlin/ie/setu/utils/DBUtilities.kt
@@ -31,11 +31,11 @@ fun mapToHealthRecord(it: ResultRow) = HealthRecord(
     sex = it[HealthRecords.sex],
 //    DOB = it[HealthRecords.DOB],
     weight = it[HealthRecords.weight],
-//    height = it[HealthRecords.height],
-//    bloodType = it[HealthRecords.bloodType],
-//    allergies = it[HealthRecords.allergies],
-//    medicalConditions = it[HealthRecords.medicalConditions],
-//    medications = it[HealthRecords.medications],
-//    notes = it[HealthRecords.notes],
+    height = it[HealthRecords.height],
+    bloodType = it[HealthRecords.bloodType],
+    allergies = it[HealthRecords.allergies],
+    medicalConditions = it[HealthRecords.medicalConditions],
+    medications = it[HealthRecords.medications],
+    notes = it[HealthRecords.notes],
     userId = it[HealthRecords.userId]
 )
