d0698a555e1cf97b05e819aadcb33a13b82cd6ab
emmaroche <20088680@mail.wit.ie>
2023-11-04 18:07:19 +0000

    Update weight goal files

--- src/main/kotlin/ie/setu/domain/db/WeightGoals.kt
+++ src/main/kotlin/ie/setu/domain/db/WeightGoals.kt
@@ -6,11 +6,11 @@ import org.jetbrains.exposed.sql.Table
 object WeightGoals : Table("weightGoals") {
     val id = integer("id").autoIncrement().primaryKey()
     val type = varchar("type", 50)
-    val startingWeight = double("starting_value")
-    val startingWeightTimestamp = varchar("start_timestamp", 100)
-    val currentWeight = double("current_value")
-    val targetWeight = double("target_value")
-    val weeklyGoal = double("weekly_goal")
+    val startingWeight = double("startingWeight")
+    val startingWeightTimestamp = varchar("startingWeightTimestamp", 100)
+    val currentWeight = double("currentWeight")
+    val targetWeight = double("targetWeight")
+    val weeklyGoal = double("weeklyGoal")
     val deadline = datetime("deadline")
     val userId = integer("user_id").references(Users.id, onDelete = ReferenceOption.CASCADE)
 }
