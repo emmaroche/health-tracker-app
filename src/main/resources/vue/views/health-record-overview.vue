2d103db76cfcc91b7bdd4d0d43f7ece48ea503a0
emmaroche <20088680@mail.wit.ie>
2023-12-22 18:33:50 +0000

    Update UX of health record vue files

--- src/main/resources/vue/views/health-record-overview.vue
+++ src/main/resources/vue/views/health-record-overview.vue
@@ -1,53 +1,197 @@
 <template id="health-record-overview">
   <app-layout>
-    <div class="card bg-light mb-3">
+    <div class="card bg-light mt-4 mb-3">
       <div class="card-header">
         <div class="row">
-          <div class="col-6" style="font-weight: 600;">
-            Health Records
-          </div>
+          <div class="col-6" style="font-weight: 600;">Health Records</div>
           <div class="col" align="right">
-            <button rel="tooltip" title="Add" class="btn btn-info btn-sm" @click="hideForm =!hideForm" style="background-color: #08a29e; border-color: #08a29e;">
+            <button
+                rel="tooltip"
+                title="Add"
+                class="btn btn-info btn-sm"
+                @click="hideForm = !hideForm"
+                style="background-color: #08a29e; border-color: #08a29e;"
+            >
               <i class="fa fa-plus" aria-hidden="true"></i> Add
             </button>
           </div>
         </div>
       </div>
-      <div class="card-body" :class="{ 'd-none': hideForm}">
+      <div class="card-body" :class="{ 'd-none': hideForm }">
         <form id="addHealthRecord">
           <div class="input-group mb-3">
             <div class="input-group-prepend">
-              <span class="input-group-text" id="input-health-record-firstName">First Name</span>
+              <span class="input-group-text custom-label" style="font-weight: 600;" id="input-health-record-firstName">First Name</span>
             </div>
-            <input type="text" class="form-control" v-model="formData.firstName" name="firstName" placeholder="First Name"/>
+            <input
+                type="text"
+                class="form-control"
+                v-model="formData.firstName"
+                name="firstName"
+                placeholder="First Name"
+            />
           </div>
           <div class="input-group mb-3">
             <div class="input-group-prepend">
-              <span class="input-group-text" id="input-health-record-lastName">Last Name</span>
+              <span class="input-group-text custom-label" style="font-weight: 600;" id="input-health-record-lastName">Last Name</span>
             </div>
-            <input type="text" class="form-control" v-model="formData.lastName" name="lastName" placeholder="Last Name"/>
+            <input
+                type="text"
+                class="form-control"
+                v-model="formData.lastName"
+                name="lastName"
+                placeholder="Last Name"
+            />
           </div>
-          <!-- Add other form fields as needed -->
+          <div class="input-group mb-3">
+            <div class="input-group-prepend">
+              <span class="input-group-text custom-label" style="font-weight: 600;" id="input-health-record-sex">Sex</span>
+            </div>
+            <input type="text" class="form-control" v-model="formData.sex" name="sex" placeholder="Sex" />
+          </div>
+          <div class="input-group mb-3">
+            <div class="input-group-prepend">
+              <span class="input-group-text custom-label" style="font-weight: 600;" id="input-health-record-dob">Date of Birth</span>
+            </div>
+            <input type="text" class="form-control" v-model="formData.dob" name="dob" placeholder="Date of Birth" />
+          </div>
+          <div class="input-group mb-3">
+            <div class="input-group-prepend">
+              <span class="input-group-text custom-label" style="font-weight: 600;" id="input-health-record-weight">Weight</span>
+            </div>
+            <input
+                type="number"
+                class="form-control"
+                v-model="formData.weight"
+                name="weight"
+                placeholder="Weight"
+            />
+          </div>
+          <div class="input-group mb-3">
+            <div class="input-group-prepend">
+              <span class="input-group-text custom-label" style="font-weight: 600;" id="input-health-record-height">Height</span>
+            </div>
+            <input
+                type="number"
+                class="form-control"
+                v-model="formData.height"
+                name="height"
+                placeholder="Height"
+            />
+          </div>
+          <div class="input-group mb-3">
+            <div class="input-group-prepend">
+              <span class="input-group-text custom-label" style="font-weight: 600;" id="input-health-record-bloodType">Blood Type</span>
+            </div>
+            <input
+                type="text"
+                class="form-control"
+                v-model="formData.bloodType"
+                name="bloodType"
+                placeholder="Blood Type"
+            />
+          </div>
+          <div class="input-group mb-3">
+            <div class="input-group-prepend">
+              <span class="input-group-text custom-label" style="font-weight: 600;" id="input-health-record-allergies">Allergies</span>
+            </div>
+            <input
+                type="text"
+                class="form-control"
+                v-model="formData.allergies"
+                name="allergies"
+                placeholder="Allergies"
+            />
+          </div>
+          <div class="input-group mb-3">
+            <div class="input-group-prepend">
+              <span class="input-group-text custom-label" style="font-weight: 600;" id="input-health-record-medicalConditions">Medical Conditions</span>
+            </div>
+            <input
+                type="text"
+                class="form-control"
+                v-model="formData.medicalConditions"
+                name="medicalConditions"
+                placeholder="Medical Conditions"
+            />
+          </div>
+          <div class="input-group mb-3">
+            <div class="input-group-prepend">
+              <span class="input-group-text custom-label" style="font-weight: 600;" id="input-health-record-medications">Medications</span>
+            </div>
+            <input
+                type="text"
+                class="form-control"
+                v-model="formData.medications"
+                name="medications"
+                placeholder="Medications"
+            />
+          </div>
+          <div class="input-group mb-3">
+            <div class="input-group-prepend">
+              <span class="input-group-text custom-label" style="font-weight: 600;" id="input-health-record-notes">Notes</span>
+            </div>
+            <input
+                type="text"
+                class="form-control"
+                v-model="formData.notes"
+                name="notes"
+                placeholder="Notes"
+            />
+          </div>
+          <div class="input-group mb-3">
+            <div class="input-group-prepend">
+              <span class="input-group-text custom-label" style="font-weight: 600;" id="input-health-record-userId">User ID</span>
+            </div>
+            <input
+                type="number"
+                class="form-control"
+                v-model="formData.userId"
+                name="userId"
+                placeholder="User ID"
+            />
+          </div>
         </form>
-        <button rel="tooltip" title="Add Health Record" class="btn btn-info btn-sm" @click="addHealthRecord" style="background-color: #08a29e; border-color: #08a29e;">
+        <button
+            rel="tooltip"
+            title="Add Health Record"
+            class="btn btn-info btn-sm mt-3"
+            @click="addHealthRecord"
+            style="background-color: #08a29e; border-color: #08a29e;"
+        >
           <i class="fa fa-plus" aria-hidden="true"></i> Add Health Record
         </button>
       </div>
     </div>
     <div class="list-group list-group-flush">
-      <div class="list-group-item d-flex align-items-start" v-for="(healthRecord, index) in healthRecords" :key="index">
+      <div
+          class="list-group-item d-flex align-items-start"
+          v-for="(healthRecord, index) in healthRecords"
+          :key="index"
+      >
         <div class="mr-auto p-2">
-          <span><a :href="`/healthRecords/${healthRecord.id}`" style="color: #08a29e;">
-            {{ healthRecord.firstName }} {{ healthRecord.lastName }} (
-            Sex: {{ healthRecord.sex }}, DOB: {{ new Date(healthRecord.dob).toLocaleDateString() }},
-            Weight: {{ healthRecord.weight }}, Height: {{ healthRecord.height }}, click to see more)
-          </a></span>
+          <span>
+            <a :href="`/healthRecords/${healthRecord.id}`" style="color: #08a29e;">
+              {{ healthRecord.firstName }} {{ healthRecord.lastName }} (
+              Sex: {{ healthRecord.sex }}, DOB: {{ new Date(healthRecord.dob).toLocaleDateString() }},
+              Weight: {{ healthRecord.weight }}, Height: {{ healthRecord.height }}, click to see more)
+            </a>
+          </span>
         </div>
         <div class="p-2">
-          <a :href="`/healthRecords/${healthRecord.id}`" class="btn btn-info btn-sm" style="background-color: #08a29e; border-color: #08a29e;">
+          <a
+              :href="`/healthRecords/${healthRecord.id}`"
+              class="btn btn-info btn-sm"
+              style="background-color: #08a29e; border-color: #08a29e;"
+          >
             <i class="fa fa-pencil" aria-hidden="true"></i>
           </a>
-          <button rel="tooltip" title="Delete" class="btn btn-danger btn-sm ml-2" @click="deleteHealthRecord(healthRecord, index)">
+          <button
+              rel="tooltip"
+              title="Delete"
+              class="btn btn-danger btn-sm ml-2"
+              @click="deleteHealthRecord(healthRecord, index)"
+          >
             <i class="fas fa-trash" aria-hidden="true"></i>
           </button>
         </div>
@@ -55,6 +199,12 @@
     </div>
   </app-layout>
 </template>
+
+<style>
+.custom-label {
+  width: 200px;
+}
+</style>
 
 <script>
 app.component("health-record-overview", {
